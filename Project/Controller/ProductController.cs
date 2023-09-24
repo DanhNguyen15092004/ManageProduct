c86e5571eb7db10d296ecf735ab98c02937a34a3
WannaSu1cide <ngdanh1509@gmail.com>
2023-09-24 22:37:16 +0700

    CRUD api

--- Project/Controller/ProductController.cs
+++ Project/Controller/ProductController.cs
@@ -6,6 +6,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
 using Project.Models;
+using System.Net.WebSockets;
 using System.Security.AccessControl;
 using static Project.Controller.ProductController;
 
@@ -26,8 +27,8 @@ namespace Project.Controller
 
 
         public record ProductItem(int Id, string ProductsName, string CurrentPrice, string ProdType);
-        public record ProductList(int Id,string ProductsName, string CurrentPrice, string ProdType,List<HistoryPrice> history);
-        public record HistoryPrice(string OldPrices, DateTime DateChange);
+        public record ProductList(int Id,string ProductsName, string CurrentPrice, string ProdType,List<HistoryPriceItem> history);
+        public record HistoryPriceItem (string OldPrices, DateTime DateChange);
 
         public class Result
         {
@@ -59,7 +60,6 @@ namespace Project.Controller
                                               a.CurrentPrice,
                                               a.ProdType
                                           )).ToListAsync();
-
                 if (productItems.Any() is false)
                 {
                     _log.LogInformation("No data to query");
@@ -70,7 +70,7 @@ namespace Project.Controller
                 {
                     var history = await (from b in _context.HistoryPrice
                                          where b.IdProduct == item.Id
-                                         select new HistoryPrice(
+                                         select new HistoryPriceItem(
                                              b.OldPrice,
                                              b.DateChange
                                          )).ToListAsync();
@@ -82,7 +82,6 @@ namespace Project.Controller
                         history
                     ));
                 }
-                _log.LogInformation("Log thanh cong ");
                 return Ok(new Result<List<ProductList>>
                 {
                     data = productLists,
@@ -97,81 +96,157 @@ namespace Project.Controller
             }
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetItem(int id)
-        {
-            var item = _context.Products.FindAsync(id);
-            if (item != null)
-            {
-                return Ok(item);
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> NewProduct([FromBody] Products prod)
+   
+        
+        public record ProductItemWithoutHistory(string ProductsName,string CurrentPrice, string ProdType,DateTime DateCreate);
+        [HttpPost("newItem")]
+        public async Task<ActionResult<Result>> CreateProduct([FromBody] Products prod)
         {
             try
             {
-                var newProduct = new Products
-                {
-                    ProductsName = prod.ProductsName,
-                    CurrentPrice = prod.CurrentPrice,
-                    ProdType = prod.ProdType
+                var item =  new Products{
+                     ProductsName=  prod.ProductsName,
+                     CurrentPrice =  prod.CurrentPrice,
+                     ProdType = prod.ProdType,
+                     DateCreate =  DateTime.Now
                 };
-
-               await _context.Products.AddAsync(newProduct);
+                await _context.Products.AddAsync(item);
                 await _context.SaveChangesAsync();
-                return Ok(newProduct);
+                return Ok(new Result { Success = " Tạo thành công " });
             }
             catch (Exception e)
             {
-                return BadRequest(e.Message);
+                _log.LogInformation($"Có lỗi nên tạo thất bại {e.Message}");
+                return Ok(new Result { Error = new (message :$"{e.Message}")});
             }
         }
+
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProduct(int id)
+        public async Task<ActionResult<Result>> DeleteProduct(int id)
         {
             try
             {
-                var deleteProduct = await _context.Products.FindAsync(id);
-                _context.Products.Remove(deleteProduct);
+                var deleteItem =await (from a in _context.Products
+                                  where a.StatusProduct == 1 && a.Id == id
+                                  select a).FirstOrDefaultAsync();
+
+
+                if (deleteItem == null) return Ok(new Result { Error = new(message : "Không tìm thấy vật phẩm ")});
+
+
+                deleteItem.StatusProduct = 0;
+              
+
+                var deleteHistoryItem =await (from b in _context.HistoryPrice
+                                         where b.StatusHistory == 1 && b.IdProduct == id
+                                         select b).FirstOrDefaultAsync();
+
+                if (deleteHistoryItem != null)
+                {
+                    deleteHistoryItem.StatusHistory = 0;   
+                }
+
+
+
                 await _context.SaveChangesAsync();
-                return Ok("DeleteSuccess");
+                return Ok(new Result { Success = "Đã xóa thành công" });
             }
             catch (Exception e)
             {
-                return BadRequest(e.Message);
-
-
+                _log.LogError(e, $"Không thể xóa sản phẩm {e.Message}");
+                return Ok(new Result { Error = new(message: "Đã có lỗi xảy ra") });
             }
         }
 
-        [HttpPost("{id}")]
-        public async Task<IActionResult> EditItem (Products product , int id)
+
+       
+        [HttpPost("editName")]
+        public async Task<ActionResult<Result>> EditName([FromBody] Products prod)
         {
-            var item = await _context.Products.FindAsync(id);
             try
             {
-                item.ProductsName = product.ProductsName;
-                item.CurrentPrice = product.CurrentPrice;
-                item.ProdType = product.ProdType;
-             await  _context.SaveChangesAsync();
-                return Ok(item);
+                var changeName = await (from a in _context.Products
+                                        where a.Id == prod.Id && a.StatusProduct == 1
+                                        select a).FirstOrDefaultAsync();
+
+
+                if (changeName == null) return Ok(new Result { Error = new(message: " Không tìm thấy sản phẩm này ") });
+
+                changeName.ProductsName = prod.ProductsName;
+                _context.SaveChanges();
+                return Ok(new Result { Success = "Thành công thay đổi tên " });
             }catch(Exception e)
             {
-                return BadRequest(e.Message);
+                _log.LogError(e, "Không thể tìm thấy sản phẩm này ");
+                return Ok(new Result { Error = new(message: "Đã có lỗi k thể tìm kiếm sản phẩm") });
             }
+          
+
+            
+
         }
 
+    
+        [HttpPost("editType")]
+        public async Task<ActionResult<Result>> EditType([FromBody] Products prod)
+        {
+            try
+            {
+                var changeType = await (from a in _context.Products
+                                        where a.Id == prod.Id && a.StatusProduct == 1
+                                        select a).FirstOrDefaultAsync();
 
 
+                if (changeType == null) return Ok(new Result { Error = new(message: " Không tìm thấy sản phẩm này ") });
+
+                changeType.ProdType = prod.ProdType;
+                _context.SaveChanges();
+                return Ok(new Result { Success = "Thành công thay đổi tên " });
+            }
+            catch (Exception e)
+            {
+                _log.LogError(e, $"Không thể tìm thấy sản phẩm này {e.Message} ");
+                return Ok(new Result { Error = new(message: "Đã có lỗi k thể tìm kiếm sản phẩm") });
+            }
+
+        }
+    
+
+        [HttpPost("editPrice")]
+
+        public async Task<ActionResult<Result>> EditPrice(Products prod) 
+        {
+            try
+            {
+            var item =await (from a in _context.Products
+                        where a.Id == prod.Id && a.StatusProduct == 1
+                        select a).FirstOrDefaultAsync();
+
+
+            if (item == null) return Ok(new Result { Error = new(message: "KHông thể tìm thấy vật phẩm này ") });
+                var newHistoryProduct = new HistoryPrice
+                {
+                    OldPrice = item.CurrentPrice,
+                    DateChange = DateTime.Now,
+                    StatusHistory = 1 , 
+                    IdProduct = prod.Id,
+                    CurrentPrice = prod.CurrentPrice
+                };
+                item.CurrentPrice = prod.CurrentPrice;
+                 await  _context.HistoryPrice.AddAsync(newHistoryProduct);
+                await  _context.SaveChangesAsync();
+                return Ok(new Result { Success = " Đã thay đổi thành công " });
+            }
+
+            catch (Exception e)
+            {
+                _log.LogError(e, $"Không thể thay đổi giá tiền của sản phẩm {e.Message}");
+                return Ok(new Result { Error = new(message: "Có sự cố về mạng hãy thử lại ") });
+            }
+        }   
 
     }
 
 
-}
 
+}
